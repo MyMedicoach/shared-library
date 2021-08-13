3f2cb81feb7621fb39f881b40159a56051f0b67b
ephys <hello@guylian.me>
2021-08-13 11:43:47 +0200

    feat: add PartialBy helper type

--- types.ts
+++ types.ts
@@ -3,3 +3,5 @@ export type Nullish<T> = T | null | undefined;
 export type NonUndefined<T> = T extends undefined ? never : T;
 
 export type NonNull<T> = T extends null ? never : T;
+
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
