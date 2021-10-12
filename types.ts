6c11d188d35eaad02e0f865d20c5dda0ef474f88
ephys <hello@guylian.me>
2021-10-12 20:52:18 +0200

    feat: add MaybeArray / ArrayElement utility types

--- types.ts
+++ types.ts
@@ -5,3 +5,8 @@ export type NonUndefined<T> = T extends undefined ? never : T;
 export type NonNull<T> = T extends null ? never : T;
 
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+export type MaybeArray<T> = T[] | T;
+
+export type ArrayElement<ArrayType extends readonly unknown[]> =
+  ArrayType extends ReadonlyArray<infer ElementType> ? ElementType : never;
