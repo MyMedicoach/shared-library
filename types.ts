49009214d3b2ef67b3edbf318ef35a2d40214a1d
ephys <hello@guylian.me>
2021-10-27 10:56:49 +0200

    feat: add isNonNullish typescript helper

--- types.ts
+++ types.ts
@@ -5,3 +5,7 @@ export type NonUndefined<T> = T extends undefined ? never : T;
 export type NonNull<T> = T extends null ? never : T;
 
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+export function isNonNullish<T>(value: T | null | undefined): value is T {
+  return value != null;
+}
