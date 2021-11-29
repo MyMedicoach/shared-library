40724176472a3a67e8c324ca57f917039979d7fe
ephys <hello@guylian.me>
2021-11-29 12:15:52 +0100

    feat: add clamp

--- number-utils.ts
+++ number-utils.ts
@@ -10,3 +10,7 @@ export function *range(start: number, end: number, step: number = 1) {
 export function padNumber(number: number | string, length: number): string {
   return String(number).padStart(length, '0');
 }
+
+export function clamp(min: number, value: number, max: number): number {
+  return Math.max(min, Math.min(max, value));
+}
