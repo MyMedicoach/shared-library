b154efca626b22c0127dc64f4ec107baf78cde88
Guylian Cox <hello@guylian.me>
2022-01-03 12:15:54 +0100

    feat: add uriTag tag

--- string-utils.ts
+++ string-utils.ts
@@ -5,3 +5,18 @@ export function pythonSplit(str: string, sep: string, splitCount?: number) {
     ? splits.slice(0, splitCount).concat([splits.slice(splitCount).join(sep)])
     : splits;
 }
+
+export function uriTag(strings: TemplateStringsArray, ...parameters: unknown[]): string {
+  let out = '';
+  let i = 0;
+
+  for (const string of strings) {
+    out += string;
+    if (parameters[i]) {
+      out += encodeURIComponent(String(parameters[i]));
+      i++;
+    }
+  }
+
+  return out;
+}
