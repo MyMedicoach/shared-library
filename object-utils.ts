889507df0cc4e26772d785d5de906aa515e313c6
ephys <zoe@ephys.dev>
2022-02-01 17:59:16 +0100

    feat: add isPojo

--- object-utils.ts
+++ object-utils.ts
@@ -3,3 +3,13 @@ export function isObject(val: any): val is object {
 }
 
 export const EMPTY_OBJECT = Object.freeze({});
+
+export function isPojo(obj: any): obj is { [key: string]: string } {
+  if (obj === null || typeof obj !== 'object') {
+    return false;
+  }
+
+  const prototype = Object.getPrototypeOf(obj);
+
+  return prototype == null || prototype === Object.prototype;
+}
